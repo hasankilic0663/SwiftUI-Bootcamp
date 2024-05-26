5ab8fd29c2a6ef91368b20afe5c84711f56977fa
hasankilic <hasankilic0663@hotmail.com>
2024-05-26 23:00:11 +0300

    commited.

--- GradientsBootcamp/GradientsBootcamp/ContentView.swift
+++ GradientsBootcamp/GradientsBootcamp/ContentView.swift
@@ -11,7 +11,9 @@ struct ContentView: View {
     var body: some View {
         RoundedRectangle(cornerRadius: 15.0)
 //            .fill(LinearGradient(colors: [.red,.blue], startPoint: .bottomLeading, endPoint: .topTrailing ))//renk geçisi
-            .fill(RadialGradient(gradient: Gradient(colors: [.blue,.purple]), center: .center, startRadius: 0, endRadius: 300))//merkez noktasından nekadar uzaklıktan baslayık nek adar uzaklıktan bıtıcek rengı ortadan
+//            .fill(RadialGradient(gradient: Gradient(colors: [.blue,.purple]), center: .center, startRadius: 0, endRadius: 300))//merkez noktasından nekadar uzaklıktan baslayık nek adar uzaklıktan bıtıcek rengı ortadan
+        
+            .fill(AngularGradient(colors: [.blue,.green,.purple], center: .bottom ))
             .frame(width: 300,height: 200)
     }
 }
