e18958185d7c58f6bdf28738b331b78bf87c803f
hasankilic <hasankilic0663@hotmail.com>
2024-05-26 11:52:33 +0300

    4.video yapıldı.

--- SwiftUIShapes/SwiftUIShapes/ContentView.swift
+++ SwiftUIShapes/SwiftUIShapes/ContentView.swift
@@ -9,9 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Circle()//çember
+        
+       // Capsule()//kapsul tarzı
+//        Ellipse() //elips
+      // Rectangle()//dikdörtgen
+        
+        RoundedRectangle(cornerRadius: 10.0)//koseleri ayarlamada ayarlı dıkdortgen
+            .frame(width: 200,height: 100)
+//            //.foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+        
+       // Circle()//çember
  //           .fill(Color.red)//arka planının rengini değiştirme
-            .foregroundColor(.orange)// buda arkasını turuncu yaptı
+            //.foregroundColor(.orange)// buda arkasını turuncu yaptı
+        //linewidth ise dış cerceve kalınlıgı
+       //    .stroke(Color.blue , lineWidth: 10.0)//içini bos yaptı mavi dış çerçeve
+//            .stroke(style:   StrokeStyle(lineWidth:10,lineCap: .round,dash: [30]))//kesikli cizgi linewidth ise cerceve kalınlıgı kesikli zaten
+//        
+//            .trim(from:0.3 , to: 4.0)//burada yuzdelık yapabılırız stroke ıle
+//            .stroke(Color.purple,lineWidth: 50)
+//        
+        
     }
 }
 
