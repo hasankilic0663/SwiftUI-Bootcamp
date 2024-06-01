51a520fa244ac5d2ccaa345fb899de97578f6032
hasankilic <hasankilic0663@hotmail.com>
2024-06-01 23:53:57 +0300

    commited.

--- SpacerBootcamp/SpacerBootcamp/ContentView.swift
+++ SpacerBootcamp/SpacerBootcamp/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing:0){//nil bos deger atamak spacing ise nesne arası oran bosluk ayarlama
+            Spacer()//Spcaer ile bosluk olusturmak
+                .frame(width: 10)
+                .background(.white)
+            Rectangle()
+                .frame(width: 50,height: 50)
+                .foregroundColor(.red)
+            Spacer(minLength: 30)//burada spacer min 10 cm uzun olacak diğerleri gbi ayarlı degıl
+                .frame(width: 10)
+                .background(.black)
+            Rectangle()
+                .frame(width: 50,height: 50)
+                .foregroundColor(.blue)
+            Spacer()//Spcaer ile bosluk olusturmak
+                .frame(width: 10)
+                .background(.white)
+            
         }
-        .padding()
+        .background(.orange)
+        .padding(.horizontal,200)
     }
 }
 
