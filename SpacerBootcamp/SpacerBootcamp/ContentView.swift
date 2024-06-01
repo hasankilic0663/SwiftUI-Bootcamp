68de8cc51f5c5a7f18b5e2a862ff6fb5c0d68e76
hasankilic <hasankilic0663@hotmail.com>
2024-06-02 00:16:28 +0300

    init and enum created.

--- SpacerBootcamp/SpacerBootcamp/ContentView.swift
+++ SpacerBootcamp/SpacerBootcamp/ContentView.swift
@@ -9,26 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing:0){//nil bos deger atamak spacing ise nesne arası oran bosluk ayarlama
-            Spacer()//Spcaer ile bosluk olusturmak
-                .frame(width: 10)
-                .background(.white)
-            Rectangle()
-                .frame(width: 50,height: 50)
-                .foregroundColor(.red)
-            Spacer(minLength: 30)//burada spacer min 10 cm uzun olacak diğerleri gbi ayarlı degıl
-                .frame(width: 10)
-                .background(.black)
-            Rectangle()
-                .frame(width: 50,height: 50)
-                .foregroundColor(.blue)
-            Spacer()//Spcaer ile bosluk olusturmak
-                .frame(width: 10)
-                .background(.white)
-            
+//        VStack(spacing:0){//nil bos deger atamak spacing ise nesne arası oran bosluk ayarlama
+//            Spacer()//Spcaer ile bosluk olusturmak
+//                .frame(width: 10)
+//                .background(.white)
+//            Rectangle()
+//                .frame(width: 50,height: 50)
+//                .foregroundColor(.red)
+//            Spacer(minLength: 30)//burada spacer min 10 cm uzun olacak diğerleri gbi ayarlı degıl
+//                .frame(width: 10)
+//                .background(.black)
+//            Rectangle()
+//                .frame(width: 50,height: 50)
+//                .foregroundColor(.blue)
+//            Spacer()//Spcaer ile bosluk olusturmak
+//                .frame(width: 10)
+//                .background(.white)
+//            
+//        }
+//        .background(.orange)
+//        .padding(.horizontal,200)
+        //-----------------------------------------------
+        VStack{
+            HStack{
+                Image(systemName: "xmark")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Spacer()
+                Image(systemName: "gear")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.blue)
+                
+                
+                
+                
+            }
+            .padding(.horizontal,20)
+            Spacer()
         }
-        .background(.orange)
-        .padding(.horizontal,200)
     }
 }
 
---
+++ initAndenum/initAndenum/initAndenumApp.swift
@@ -0,0 +1,17 @@
+//
+//  initAndenumApp.swift
+//  initAndenum
+//
+//  Created by Hasan on 2.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct initAndenumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
