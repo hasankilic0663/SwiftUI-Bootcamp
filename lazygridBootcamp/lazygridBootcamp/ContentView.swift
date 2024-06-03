a60d72fb83072e3bac1d5e9cfe6d6291eed4f897
hasankilic <hasankilic0663@hotmail.com>
2024-06-03 09:54:58 +0300

    lazygrid created

---
+++ lazygridBootcamp/lazygridBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lazygridBootcamp
+//
+//  Created by Hasan on 3.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lazygridBootcamp/lazygridBootcamp/lazygridBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  lazygridBootcampApp.swift
+//  lazygridBootcamp
+//
+//  Created by Hasan on 3.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct lazygridBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
